c96e81ea2a3059fa029b9cbc1e8775601cd67ea6
Jordan Cotter <jrdn.cttr@gmail.com>
2022-06-05 23:36:44 -0500

    fix: fix length of being in debt being off minimum payment wasn't being included in payment on main loan. Only extra payments, so the length to pay off debt was longer than it should

--- in-debt-cision.hs
+++ in-debt-cision.hs
@@ -10,19 +10,19 @@ interest :: Principle -> Rate -> Float
 interest p r = r/12*p
 
 applyBigPayment :: Float -> Loan -> Loan 
-applyBigPayment payment (Loan p r mp tm ti) = (Loan (p-(payment-i)) r mp (tm+1) (ti+i))
-	where i = interest p r
+applyBigPayment payment (Loan p r mp tm ti) = (Loan (p-((mp+payment)-i)) r mp (tm+1) (ti+i))
+  where i = interest p r
 
 applyMinPayment :: Loan -> Loan 
 applyMinPayment (Loan p r mp tm ti) 
-	| p == 0 = (Loan p r mp tm ti)
-	| p <= mp = (Loan 0 r mp (tm+1) (ti+i)) 
-	| otherwise = (Loan (p-(mp-i)) r mp (tm+1) (ti+i)) 
-		where i = interest p r
+  | p == 0 = (Loan p r mp tm ti)
+  | p <= mp = (Loan 0 r mp (tm+1) (ti+i)) 
+  | otherwise = (Loan (p-(mp-i)) r mp (tm+1) (ti+i)) 
+    where i = interest p r
 
 applyLastPayment :: Loan -> Loan 
-applyLastPayment (Loan p r mp tm ti) = (Loan 0.0 r mp (tm+1) (ti+i))	
-	where i = interest p r
+applyLastPayment (Loan p r mp tm ti) = (Loan 0.0 r mp (tm+1) (ti+i))  
+  where i = interest p r
 
 principle :: Loan -> Principle
 principle (Loan p _ _ _ _) = p
@@ -33,12 +33,12 @@ minpayment (Loan _ _ mp _ _) = mp
 payOffLoans :: Float -> [Loan] -> [Loan]
 payOffLoans extraCash [] = []  
 payOffLoans extraCash (l:ls)  
-	| principle l <= 0 = (l:ls) -- if principle is zero we're back to the first loan and all is paid off 
-	| principle l <= extraCash = payOffLoans (extraCash + minpayment l) ((map applyMinPayment ls) ++ [(applyLastPayment l)]) --put paid off loan at end of list 
-	| otherwise = payOffLoans extraCash ((applyBigPayment extraCash l):map applyMinPayment ls)
+  | principle l <= 0 = (l:ls) -- if principle is zero we're back to the first loan and all is paid off 
+  | principle l <= extraCash = payOffLoans (extraCash + minpayment l) ((map applyMinPayment ls) ++ [(applyLastPayment l)]) --put paid off loan at end of list 
+  | otherwise = payOffLoans extraCash ((applyBigPayment extraCash l):map applyMinPayment ls) -- same as (l:ls)
 
 mainFunc :: Float -> [Loan] -> String 
 mainFunc xc ls = (output . map outcome . payOffLoans xc) ls
-	where outcome (Loan _ _ _ tm ti) = (tm, ti); 
-	      output xs = "It would take you " ++ show (fst (last xs)) ++ " months to pay off your debts and cost you $" ++ show (foldr (\(_, i) acc -> acc+i) 0 xs) ++ 
-	      	" if you did the debt snowball in the given order. Reorder your loans to see if there's a more efficient way!"
+  where outcome (Loan _ _ _ tm ti) = (tm, ti); 
+        output xs = "It would take you " ++ show (fst (last xs)) ++ " months to pay off your debts and cost you $" ++ show (foldr (\(_, i) acc -> acc+i) 0 xs) ++ 
+          " if you did the debt snowball in the given order. Reorder your loans to see if there's a more efficient way!"
